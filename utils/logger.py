e204f238d7bd57c63042b7587670e9cbfc99ed17
AhmadM-DL <amm90@mail.aub.edu>
2020-02-23 11:18:11 +0200

    Added tf v2 support for Logger

--- utils/logger.py
+++ utils/logger.py
@@ -8,10 +8,13 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(name=tag, data=value, step=step)
+        self.writer.flush()    
+
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
-        self.writer.add_summary(summary, step)
+        for tag, value in tag_value_pairs:
+            self.scalar_summary(tag, value, step)
+
