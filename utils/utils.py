54ba62600751308d23a3369f097a8cc8698d076c
AhmadM-DL <52525688+AhmadM-DL@users.noreply.github.com>
2020-02-27 14:57:47 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -429,7 +429,6 @@ def generate_yolo_train_test_files(images_dir, output_dir, classes, train_valid_
 def replace_class_yolo_format(original_class, replace_class, images_labels_dir, image_label_file_regex=".*.txt$"):
     for file in os.listdir(images_labels_dir):
         if re.match(pattern=image_label_file_regex, string=file):
-            all_imgs+=1
 
             # open file
             f = open(os.path.join(images_labels_dir, file), "r")
